acc6b197d1afa1f0d3bbcff712d7309c6fb3d7d6
jlkerches2 <64588821+jlkerches2@users.noreply.github.com>
2023-09-06 14:47:39 +1000

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.header('🥗 🐔 🥑Breakfast Menu')
 streamlit.text('🥑Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 🐔 🥑Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞 🐔Hard-Boiled Free-Range Egg')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
